a09af0c6f08402a159f858805c456e6cb872fb1c
Eva P <eva.pelko@gmail.com>
2024-05-26 17:29:43 +0200

    add double jump, duck collisionshape changes

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,10 +5,23 @@ extends Area2D
 func _on_body_entered(body):
 	print("you died")
 	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
 
+	# Disable the collision shapes
+	call_deferred("disable_collision_shapes", body)
+
+	timer.start()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
+
+func disable_collision_shapes(body):
+	# Check and disable the normal collision shape
+	var normal_collision_shape = body.get_node_or_null("NormalCollisionShape2D")
+	if normal_collision_shape:
+		normal_collision_shape.set_deferred("disabled", true)
+
+	# Check and disable the duck collision shape
+	var duck_collision_shape = body.get_node_or_null("DuckCollisionShape2D")
+	if duck_collision_shape:
+		duck_collision_shape.set_deferred("disabled", true)
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,21 +3,34 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var can_doublejump = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var normal_collision_shape = $NormalCollisionShape2D
+@onready var duck_collision_shape = $DuckCollisionShape2D
+
+func _ready():
+	# Ensure the normal collision shape is visible and the duck collision shape is hidden at the start
+	normal_collision_shape.visible = true
+	duck_collision_shape.visible = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
+	# Handle Jump and Double Jump.
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+			can_doublejump = true
+	else:
+		if Input.is_action_just_pressed("jump") and can_doublejump:
+			velocity.y = JUMP_VELOCITY
+			can_doublejump = false
 
 	# get the input direction -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -31,14 +44,22 @@ func _physics_process(delta):
 	# play animation
 	if is_on_floor():
 		if direction == 0:
-			if Input.is_action_pressed("duck") and is_on_floor():
+			if Input.is_action_pressed("duck"):
 				animated_sprite.play("duck")
+				normal_collision_shape.visible = false
+				duck_collision_shape.visible = true
 			else:
 				animated_sprite.play("idle")
+				normal_collision_shape.visible = true
+				duck_collision_shape.visible = false
 		else: 
 			animated_sprite.play("run")
+			normal_collision_shape.visible = true
+			duck_collision_shape.visible = false
 	else:
 		animated_sprite.play("jump")
+		normal_collision_shape.visible = true
+		duck_collision_shape.visible = false
 
 	
 	# apply movement
