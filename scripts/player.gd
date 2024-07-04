563ff579808dad1dbe73ed74f9a9c007627c79ac
Eva P <eva.pelko@gmail.com>
2024-07-04 16:29:47 +0200

    add coyote time, buffer jump, early fall, anti gravity apex to movement and tileset update

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,19 @@ const SPEED = 120.0
 const JUMP_VELOCITY = -280.0
 const FALL_MULTIPLIER = 2.5  # Gravity multiplier for falling
 const FALL_ANIMATION_THRESHOLD = 0.1  # Time in seconds before the fall animation plays
+const EARLY_FALL_MULTIPLIER = 5.0  # Gravity multiplier for early fall
+const ANTI_GRAVITY_APEX_THRESHOLD = 30.0  # Velocity threshold for anti-gravity apex
+const ANTI_GRAVITY_APEX_MULTIPLIER = 0.5  # Gravity multiplier at the apex of the jump
 
 const DASH_SPEED = 400.0
 const DASH_DURATION = 0.2  # Duration of the dash in seconds
 const DASH_COOLDOWN = 0.5  # Cooldown time between dashes
+
+const COYOTE_TIME = 0.2  # Time allowed to jump after running off a ledge
+const JUMP_BUFFER_TIME = 0.2  # Time window to buffer the jump input
+
+var coyote_timer = 0.0
+var jump_buffer_timer = 0.0
 
 var gravity: float
 var can_doublejump = false
@@ -35,6 +44,10 @@ func _physics_process(delta):
 			fall_time += delta
 			if fall_time > FALL_ANIMATION_THRESHOLD:
 				animated_sprite.play("fall")
+		elif abs(velocity.y) < ANTI_GRAVITY_APEX_THRESHOLD:  # Near the apex of the jump
+			velocity.y += gravity * ANTI_GRAVITY_APEX_MULTIPLIER * delta
+			animated_sprite.play("jump")
+			fall_time = 0.0  # Reset fall time when rising
 		else:  # Rising or at the apex
 			velocity.y += gravity * delta
 			animated_sprite.play("jump")
@@ -43,14 +56,40 @@ func _physics_process(delta):
 		velocity.y = 0
 		fall_time = 0.0  # Reset fall time when on the floor
 
+	# Check if the player is grounded
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+		# Check if there is a buffered jump
+		if jump_buffer_timer > 0:
+			velocity.y = JUMP_VELOCITY
+			jump_buffer_timer = 0
+			can_doublejump = true  # Allow double jump after buffered jump
+	else:
+		coyote_timer -= delta
+
 	# Handle Jump and Double Jump
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
 			can_doublejump = true
+	elif coyote_timer > 0 and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+		coyote_timer = 0  # Reset coyote timer after jumping
+		can_doublejump = true  # Allow double jump after coyote jump
 	elif Input.is_action_just_pressed("jump") and can_doublejump:
 		velocity.y = JUMP_VELOCITY
 		can_doublejump = false
+	elif Input.is_action_just_pressed("jump"):
+		# Buffer the jump if in the air and not able to jump
+		jump_buffer_timer = JUMP_BUFFER_TIME
+
+	# Early fall handling
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y += gravity * EARLY_FALL_MULTIPLIER * delta
+
+	# Decrease jump buffer timer
+	if jump_buffer_timer > 0:
+		jump_buffer_timer -= delta
 
 	# Handle Dash
 	if is_dashing:
