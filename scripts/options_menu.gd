756733171b1e084966db6dae71187a59ef69da29
Eva P <eva.pelko@gmail.com>
2024-07-24 01:10:55 +0200

    add volume sliders to option menu

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -1,6 +1,30 @@
 extends Control
 
+@onready var sfxVolumeSlider = $"VBoxContainer/SFX/MarginContainer/HSlider"
+@onready var bgmVolumeSlider = $"VBoxContainer/Music/MarginContainer/HSlider"
+@onready var sfxPreview = $"SFX Preview"
 
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	
+
+func _on_sfx_volume_signal(new_value):
+	sfxVolumeSlider.value = new_value
+
+
+func _on_bgm_volume_signal(new_value):
+	bgmVolumeSlider.value = new_value
+
+
+
+
+func _on_sfx_volume_slider_value_changed(value):
+	Options.sfx_volume = value
+	
+	#if (visible):
+	#	sfxPreview.play()
+
+
+func _on_bgm_volume_slider_value_changed(value):
+	Options.bgm_volume = value
